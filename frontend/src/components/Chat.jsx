ed14dc5dddccdc45b550d3689fbf93f40e5a885b
Calin <cotuna.calin@yahoo.com>
2024-11-21 11:57:16 +0200

    Successfully wrote in realtime db

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import app from '../firebase/firebaseConfig';
+import { getDatabase, ref, set, push } from 'firebase/database';
 
 function Chat() {
+  let [inputValue1, setInputValue1] = useState('');
+  let [inputValue2, setInputValue2] = useState('');
+
+  const saveData = async () => {
+    const db = getDatabase(app);
+    const newDoc = push(ref(db, 'chat/test'));
+    await set(newDoc, {
+      message: inputValue1,
+      user: inputValue2,
+    }).then(() => {
+      alert('Data saved successfully');
+    }).catch((error) => {
+      alert('Data could not be saved' + error)});
+  }
   return (
     <div>
-    Chat
+    Write
+    <input
+      type="text"
+      value={inputValue1}
+      onChange={(e) => setInputValue1(e.target.value)}
+    />
+    <br />
+    <input
+      type="text"
+      value={inputValue2}
+      onChange={(e) => setInputValue2(e.target.value)}
+    />
+  <button onClick={saveData}>Save data</button>
     </div>
   );
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -14,5 +18,15 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  define: {
+    "process.env.REACT_APP_FIREBASE_API_KEY": JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+    "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+    "process.env.REACT_APP_FIREBASE_DATABASE_URL": JSON.stringify(process.env.REACT_APP_FIREBASE_DATABASE_URL),
+    "process.env.REACT_APP_FIREBASE_PROJECT_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+    "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+    "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+    "process.env.REACT_APP_FIREBASE_APP_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+    "process.env.REACT_APP_FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID),
   },
 })
