771a9a51ad9c9de7246cf972727a245920286a1d
Calin <cotuna.calin@yahoo.com>
2024-11-23 08:41:23 +0200

    Made the homepage navigation responsive

--- frontend/src/components/layout/navigation/HomepageNavigation.jsx
+++ frontend/src/components/layout/navigation/HomepageNavigation.jsx
@@ -1,31 +1,56 @@
-import React from 'react';
-import logo from '../../../assets/logo/gmailLogo.svg'
+import React, { useState, useEffect } from 'react';
+import logo from '../../../assets/logo/gmailLogo.svg';
 import { Link } from 'react-router-dom';
-
 import CreateAccountBtn from '../../ui/CreateAccountBtn';
 
 function HomepageNavigation() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 500) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <nav className='shadow-lg px-4 py-2'>
-            <div className='flex justify-between items-center'>
-                <Link to={'/'} >
+        <nav className="shadow-lg px-4 py-2 w-full bg-white z-50 fixed top-0 left-0">
+            <div className="flex justify-between items-center">
+                <Link to={'/'}>
                     <div className="flex items-center">
                         <img src={logo} alt="Gmail Logo" className="w-7 h-7 ms-2 mr-2" />
                         <span className="text-xl text-gray-500 font-light">Gmail</span>
                     </div>
                 </Link>
-                <ul className='flex space-x-5 items-center'>
-                    <li className='text-blue-500'>
-                        <Link>For work</Link>
-                    </li>
-                    <li className='text-blue-500 py-2 px-7 border rounded-md'>
+
+                <ul className="flex items-center space-x-5"> 
+                    <li className={`block md:hidden text-blue-500 py-2 px-7 border rounded-md ${isScrolled ? 'hidden' : ''}`}>
                         <Link to={'/login'}>Sign In</Link>
+                    </li>   
+                    <li className="hidden md:block text-blue-500">
+                        <Link>For work</Link>
+                    </li>                
+                    <li className="hidden md:block text-blue-500 py-2 px-7 border rounded-md">
+                        <Link to={'/login'}>Sign In</Link>
+                    </li>              
+                    <li className={`hidden md:block ${isScrolled ? '' : 'hidden'}`}>
+                        <CreateAccountBtn />
+                    </li>             
+                    <li className={`block md:hidden ${isScrolled ? '' : 'hidden'}`}>
+                        <CreateAccountBtn />
                     </li>
-                    <CreateAccountBtn />
                 </ul>
             </div>
         </nav>
     );
-};
+}
 
 export default HomepageNavigation;
--- frontend/src/components/ui/CreateAccountBtn.jsx
+++ frontend/src/components/ui/CreateAccountBtn.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
 function CreateAccountBtn() {
     return (
-        <div className='text-white bg-blue-500 py-2 px-4 pe-2 w-fit border rounded-md flex items-center justify-evenly'>
-            <Link>Create an account</Link>
-            <MdOutlineArrowDropDown className="ms-2 text-2xl" />
+        <div className="text-white bg-blue-500 py-3 px-4 w-fit border rounded-md flex items-center justify-evenly text-xs sm:text-sm md:text-base">
+            <Link className="text-xs sm:text-sm md:text-base">Create an account</Link>
+            <MdOutlineArrowDropDown className="ms-1 text-base sm:text-lg md:text-2xl" />
         </div>
     );
-};
+}
 
 export default CreateAccountBtn;
